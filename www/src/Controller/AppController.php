b59cf58c449c641ada935b1ff0d88e5fabe03596
dkantikorn <dk_antikorn@hotmail.com>
2017-11-03 07:15:12 +0700

    upload file

--- www/src/Controller/AppController.php
+++ www/src/Controller/AppController.php
@@ -58,12 +58,12 @@ class AppController extends Controller {
         $this->loadComponent('RequestHandler');
         $this->loadComponent('Flash');
         $this->loadComponent('Auth', [
-            'loginRedirect' => ['controller' => 'Users','action' => 'index'],
-            'logoutRedirect' => ['controller' => 'Users','action' => 'login'],
+            'loginRedirect' => ['controller' => 'Users', 'action' => 'index'],
+            'logoutRedirect' => ['controller' => 'Users', 'action' => 'login'],
             'authenticate' => [
-                'Form' => ['fields' => ['username' => 'username','password' => 'password']]
+                'Form' => ['fields' => ['username' => 'username', 'password' => 'password']]
             ],
-            'loginAction' => ['controller' => 'Users','action' => 'login'],
+            'loginAction' => ['controller' => 'Users', 'action' => 'login'],
             'authorize' => ['Controller'],
             'unauthorizedRedirect' => $this->referer()// If unauthorized, return them to page they were just on
         ]);
@@ -106,6 +106,90 @@ class AppController extends Controller {
     }
 
     /**
+     *
+     * This function for uload attachment excel file  from view of information style list
+     * @author  sarawutt.b
+     * @param   string name of target upload path
+     * @param   array() file attribute option from upload form
+     * @since   2017/10/30
+     * @return  array()
+     */
+    function uploadFiles($folder, $file, $itemId = null) {
+        $folder_url = WWW_ROOT . $folder;
+        $rel_url = $folder;
+
+        if (!is_dir($folder_url)) {
+            mkdir($folder_url);
+        }
+
+        //Bould new path if $itemId to be not null
+        if ($itemId) {
+            $folder_url = WWW_ROOT . $folder . '/' . $itemId;
+            $rel_url = $folder . '/' . $itemId;
+            if (!is_dir($folder_url)) {
+                mkdir($folder_url);
+            }
+        }
+
+        //define for file type where it allow to upload
+        $map = [
+            'image/gif' => '.gif',
+            'image/jpeg' => '.jpg',
+            'image/png' => '.png',
+            'application/pdf' => '.pdf',
+            'application/msword' => '.doc',
+            'application/vnd.openxmlformats-officedocument.wordprocessingml.document' => '.docx',
+            'application/excel' => '.xls',
+            'application/vnd.ms-excel' => '.xls',
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' => '.xlsx'
+        ];
+
+        //Bould file extension keep to the database
+        $userfile_extn = substr($file['name'], strrpos($file['name'], '.') + 1);
+        if (array_key_exists($file['type'], $map)) {
+            $typeOK = true;
+        }
+
+        //Rename for the file if not change of the upload file makbe duplicate
+        $filename = $this->VERSION() . $map[$file['type']];
+        if ($typeOK) {
+            switch ($file['error']) {
+                case 0:
+                    @unlink($folder_url . '/' . $filename); //Delete the file if it already existing
+                    $full_url = $folder_url . '/' . $filename;
+                    $url = $rel_url . '/' . $filename;
+                    $success = move_uploaded_file($file['tmp_name'], $url);
+                    if ($success) {
+                        $result['urls'][] = '/' . $url;
+                        $result['upfilename'][] = $filename;
+                        $result['ext'][] = $userfile_extn;
+                        $result['origin_name'][] = $file['name'];
+                        $result['type'][] = $file['type'];
+                    } else {
+                        $result['errors'][] = __("Error uploaded {$filename}. Please try again.");
+                    }
+                    break;
+                case 3:
+                    $result['errors'][] = __("Error uploading {$filename}. Please try again.");
+                    break;
+                case 4:
+                    $result['nofiles'][] = __("No file Selected");
+                    break;
+                default:
+                    $result['errors'][] = __("System error uploading {$filename}. Contact webmaster.");
+                    break;
+            }
+        } else {
+            $permiss = '';
+            foreach ($map as $k => $v) {
+                $permiss .= "{$v}, ";
+            }
+            $result['errors'][] = __("{$filename} cannot be uploaded. Acceptable file types in : %s", trim($permiss, ','));
+        }
+        return $result;
+    }
+
+    /**
      * Before render callback.
      *
      * @param \Cake\Event\Event $event The beforeRender event.
@@ -120,6 +204,28 @@ class AppController extends Controller {
         ) {
             $this->set('_serialize', true);
         }
+    }
+
+    /**
+     *
+     * Function used fro generate _VERSION_
+     * @author  sarawutt.b
+     * @return  biginteger of the version number
+     */
+    public function VERSION() {
+        $parts = explode(' ', microtime());
+        $micro = $parts[0] * 1000000;
+        return(substr(date('YmdHis'), 2) . sprintf("%06d", $micro));
+    }
+
+    /**
+     *
+     * Function used for generate UUID key patern
+     * @author  sarawutt.b
+     * @return  string uuid in version
+     */
+    function UUID() {
+        return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x', mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0x0fff) | 0x4000, mt_rand(0, 0x3fff) | 0x8000, mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff));
     }
 
 }
